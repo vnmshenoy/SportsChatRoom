5a52b48a477fdd35b29db1c68163efaa8d1bf6c2
manjunath.shenoy <vnmshenoy@gmail.com>
2018-11-21 12:12:02 +1100

    initial draft

---
+++ SportsChatRoom/MainHub.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+using Microsoft.AspNet.SignalR.Hubs;
+
+namespace SportsChatRoom
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int  ConnectionId { get; set; }
+    }
+    [HubName("sportschatroom")]
+    public class MainHub : Hub
+    {
+        public List<User> ConnectedUsers = new List<User>();
+        public List<User> messages = new List<User>();
+        
+        //Client calls this method to join the room. There will be other clients as well in that room
+        public void JoinSportRoom(string room,string name)
+        {
+            Groups.Add(Context.ConnectionId, room);
+            Clients.OthersInGroup(room).newUser(name);         
+        }
+        public  Task OnConnected() { return null; }
+
+        public  Task OnDisconnected() { return null; }
+        public  Task OnReconnected() { return null; }
+    }
+}
---
+++ SportsChatRoom/Startup1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(SportsChatRoom.Startup1))]
+
+namespace SportsChatRoom
+{
+    public class Startup1
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            
+                app.MapSignalR();
+            
+        }
+    }
+}
